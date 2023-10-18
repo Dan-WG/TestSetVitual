17b3cbb6d0d7987277150a2a54686469d518c2b0
GustavoMendozaAren <stavmdza@gmail.com>
2023-10-18 14:38:43 -0600

    YBotAnimations&ConsoleReferences

--- Assets/Scripts/1-NuevosScripts/GeneralController.cs
+++ Assets/Scripts/1-NuevosScripts/GeneralController.cs
@@ -7,10 +7,12 @@ using TMPro;
 
 public class GeneralController : MonoBehaviour
 {
-    public GameObject rielMovY, motorMovX, tubosMovZ;
-    public TMP_InputField inputY;
+    public GameObject rielMovY, motorMovX, tubosMovZ1, tubosMovZ2;
+    public TMP_InputField inputY, inputX, inputZ;
 
-    private int x, y, z;
+    private float x, y, z;
+
+    private float z1, z2;
 
     private int limintY;
 
@@ -22,19 +24,48 @@ public class GeneralController : MonoBehaviour
     }*/
     public void InputY()
     {
-        inputYbool = true;
-        y = Convert.ToInt32(inputY.text);
-        //rielMovY.transform.localPosition = new Vector3(0, 0, y);
-        //Debug.Log(inputY.text);
+        y = float.Parse(inputY.text);
+        //rielMovY.transform.localPosition = new Vector3(0, 0, -y);
+        //Debug.Log(y);
+    }
+
+    public void InputX() 
+    {
+        x = float.Parse(inputX.text);
+    }
+    public void InputZ() 
+    { 
+        z = float.Parse(inputZ.text);
     }
 
     private void Update()
     {
-        if (inputYbool) 
+        if (y >= 0)
         {
-            Debug.Log(inputYbool);
-            //if(limintY < y)
-            //rielMovY.transform.localPosition += new Vector3(0, 0, y);
+            rielMovY.transform.localPosition = Vector3.Lerp(rielMovY.transform.localPosition, new Vector3(0,0,-y), 0.01f);
         }
+        if (x >= 0)
+        {
+            motorMovX.transform.localPosition = Vector3.Lerp(motorMovX.transform.localPosition, new Vector3(x, 0, 0), 0.01f);
+        }
+        if (z > 1.5f)
+        {
+            z1 = 1.5f;
+            z2 = z - 1.5f;
+            if (tubosMovZ1.transform.localPosition.z < z1)
+            {
+                tubosMovZ1.transform.localPosition = Vector3.Lerp(tubosMovZ1.transform.localPosition, new Vector3(0, -z1, 0), 0.01f);
+                tubosMovZ2.transform.localPosition = Vector3.Lerp(tubosMovZ2.transform.localPosition, new Vector3(0, -z2, 0), 0.01f);
+            }
+        }
+        else if (z < 1.5f)
+        {
+            tubosMovZ1.transform.localPosition = Vector3.Lerp(tubosMovZ1.transform.localPosition, new Vector3(0, -z, 0), 0.01f);
+            tubosMovZ2.transform.localPosition = Vector3.Lerp(tubosMovZ2.transform.localPosition, new Vector3(0, 0, 0), 0.01f);
+        }
+        //else if (rielMovY.transform.localPosition.y > -y)
+        //{
+        //    rielMovY.transform.localPosition += new Vector3(0, 0, Time.deltaTime * 2f);
+        //}
     }
 }
